3b2d07964b6a0b396edf1d916566cd7eb1b54749
SiN0102 <73993213+SiN0102@users.noreply.github.com>
2021-01-18 19:09:34 +0300

    Add files via upload

---
+++ LR1/2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ЛабТП1.Kontroler;
+using ЛабТП1.Model;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        order oformlenie = new order();
+        menu todayMenu;
+        int Mode;
+        public MainWindow()
+        {
+            todayMenu = new menu();
+
+            InitializeComponent();
+
+            MenuView.ItemsSource = todayMenu.MyMenu;
+            OrderList.ItemsSource = oformlenie.C;
+            AllOrders.ItemsSource = oformlenie.D;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuView.SelectedItem == null)
+            {
+                MessageBox.Show("Выберите блюдо из меню");
+            }
+            else
+                try
+                {
+                    bool v = oformlenie.ChoseItem((interfacefood)MenuView.SelectedItem, Convert.ToInt32(Kolvo.Text));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Введите количество с помощью цифр");
+                }
+            OrderList.Items.Refresh();
+        }
+
+        private void EndOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (OrderList.Items.Count == 0)
+            {
+                MessageBox.Show("Выберите блюдо из меню");
+            }
+            else
+                try
+                {
+                    oformlenie.FinishOrder();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Выберите блюдо из меню");
+                }
+            AllOrders.Items.Refresh();
+        }
+
+        private void ClearOrderClick(object sender, RoutedEventArgs e)
+        {
+            if (OrderList.Items.Count == 0)
+            {
+                MessageBox.Show("Выберите блюдо из меню");
+            }
+            else
+                try
+                {
+                    OrderList.ItemsSource = oformlenie.C;
+                }
+                catch
+                {
+                    MessageBox.Show("Выберите блюдо из меню");
+                }
+        }
+
+
+        private void MenuView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void NextList(object sender, RoutedEventArgs e)
+        {
+            MenuView.ItemsSource = todayMenu.MyMenu;
+            switch (Mode)
+            {
+                case 0:
+                    {
+                        Mode = 1;
+                        todayMenu = new menu(Mode);
+                        MenuView.ItemsSource = todayMenu.MyMenu;
+                        break;
+                    }
+
+                case 1:
+                    {
+                        Mode = 2;
+                        todayMenu = new menu(Mode);
+                        MenuView.ItemsSource = todayMenu.MyMenu;
+                        break;
+                    }
+                case 2:
+                    {
+                        Mode = 3;
+                        todayMenu = new menu(Mode);
+                        MenuView.ItemsSource = todayMenu.MyMenu;
+                        break;
+                    }
+                case 3:
+                    {
+                        Mode = 4;
+                        todayMenu = new menu(Mode);
+                        MenuView.ItemsSource = todayMenu.MyMenu;
+                        break;
+                    }
+                case 4:
+                    {
+                        Mode = 0;
+                        todayMenu = new menu(Mode);
+                        MenuView.ItemsSource = todayMenu.MyMenu;
+                        break;
+                    }
+            }
+        }
+
+        private void OldOrderView_Click(object sender, RoutedEventArgs e)
+        {
+                if (AllOrders.SelectedItem == null)
+                {
+                    MessageBox.Show("Выберите заказ из списка");
+                }
+                else
+                    try
+                    {
+                        OrderList.ItemsSource = ((total)AllOrders.SelectedItem).orderlist;
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Выберите заказ из списка");
+                    }
+            }
+
+        private void AllOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+    }
---
+++ LR1/2/WpfApp1/model/menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ЛабТП1.Model
+{
+    public class menu
+    {
+        public List<interfacee> Menu = new List<interfacee>();
+
+        public menu()
+        {
+            interfacee cake = new desert("пудинг", 300, 200, 15, 250);
+            interfacee icecream = new desert("сорбет", 300, 100, 5, 100);
+            interfacee pie = new desert("тирамису", 350, 250, 20, 250);
+            interfacee chocolate = new desert("белый шоколад", 100, 50, 5, 100);
+
+            interfacee juice = new drink("сок", 150, 40, 10, 200);
+            interfacee tea = new drink("чай", 100, 40, 10, 200);
+            interfacee coffe = new drink("кофе", 200, 40, 10, 200);
+            interfacee latte = new drink("латте", 200, 40, 10, 200);
+
+            interfacee ceasar = new salat("цезарь", 360, 200, 15, 250);
+            interfacee light = new salat("легкий", 300, 200, 15, 250);
+            interfacee olivier = new salat("оливье", 300, 200, 15, 250);
+            interfacee hot = new salat("горячий салат", 300, 200, 15, 250);
+
+            interfacee kanape = new coldsnack("канапе", 150, 40, 10, 200);
+            interfacee pashtet = new coldsnack("паштет", 200, 40, 10, 200);
+            interfacee cheese = new coldsnack("сырные шарики", 90, 40, 10, 200);
+            interfacee meet = new coldsnack("мясной рулет", 300, 200, 10, 200);
+
+            Menu.Add(cake);
+            Menu.Add(icecream);
+            Menu.Add(pie);
+            Menu.Add(chocolate);
+            Menu.Add(coffe);
+            Menu.Add(tea);
+            Menu.Add(juice);
+            Menu.Add(latte);
+            Menu.Add(ceasar);
+            Menu.Add(light);
+            Menu.Add(olivier);
+            Menu.Add(hot);
+            Menu.Add(kanape);
+            Menu.Add(pashtet);
+            Menu.Add(cheese);
+            Menu.Add(meet);
+        }
+    }
+}
